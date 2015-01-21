d553cf4099a1a5408adba4d3c066fd5a732fb2dd
Jonathan Boulle <jonathanboulle@gmail.com>
2015-01-21 15:22:29 -0800

    server: Stop -> Kill
    To make things a little clearer for ol' man Crawford, rename the "Stop"
    function to "Kill" to align better with the channel names and be a
    little more explicit that it is invoked in response to a kill signal.

--- fleetd/fleetd.go
+++ fleetd/fleetd.go
@@ -120,7 +120,7 @@ func main() {
 		}
 
 		log.Infof("Restarting server components")
-		srv.Stop()
+		srv.Kill()
 
 		srv, err = server.New(*cfg)
 		if err != nil {
@@ -135,7 +135,7 @@ func main() {
 		srvMutex.Lock()
 		defer srvMutex.Unlock()
 
-		srv.Stop()
+		srv.Kill()
 		srv.Purge()
 		os.Exit(0)
 	}
