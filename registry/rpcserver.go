b506ec707d9ae4effeb18014c7a51966c028d280
Hugo Tavares Reis <hugo.t.r@gmail.com>
2015-11-29 18:58:24 +0100

    added some instrumentation to rpcserver

--- registry/rpcserver.go
+++ registry/rpcserver.go
@@ -8,6 +8,7 @@ import (
 
 	"golang.org/x/net/context"
 
+	"github.com/coreos/fleet/debug"
 	pb "github.com/coreos/fleet/rpc"
 	"google.golang.org/grpc"
 
@@ -56,14 +57,14 @@ func (r *RPCRegistry) startServer() {
 }
 
 func (s *rpcserver) GetScheduledUnits(ctx context.Context, unitFilter *pb.UnitFilter) (*pb.ScheduledUnits, error) {
-	fmt.Println("XENGINE", "GetScheduledUnits", unitFilter)
+	defer debug.Exit_(debug.Enter_())
 	units, err := s.localRegistry.Schedule()
 
 	return &pb.ScheduledUnits{Units: units}, err
 }
 
 func (s *rpcserver) GetScheduledUnit(ctx context.Context, name *pb.UnitName) (*pb.MaybeScheduledUnit, error) {
-	fmt.Println("XENGINE", "GetScheduledUnit", name)
+	defer debug.Exit_(debug.Enter_(name.Name))
 
 	su, exists := s.localRegistry.ScheduledUnit(name.Name)
 	if exists {
@@ -73,7 +74,7 @@ func (s *rpcserver) GetScheduledUnit(ctx context.Context, name *pb.UnitName) (*p
 }
 
 func (s *rpcserver) GetUnit(ctx context.Context, name *pb.UnitName) (*pb.MaybeUnit, error) {
-	fmt.Println("XENGINE", "GetUnit", name)
+	defer debug.Exit_(debug.Enter_(name.Name))
 
 	unit, exists := s.localRegistry.Unit(name.Name)
 	if exists {
@@ -84,26 +85,30 @@ func (s *rpcserver) GetUnit(ctx context.Context, name *pb.UnitName) (*pb.MaybeUn
 }
 
 func (s *rpcserver) GetUnits(ctx context.Context, filter *pb.UnitFilter) (*pb.Units, error) {
-	fmt.Println("XENGINE", "GetUnits", filter)
+	defer debug.Exit_(debug.Enter_())
+
 	units := s.localRegistry.Units()
 	return &pb.Units{Units: units}, nil
 }
 
 func (s *rpcserver) GetUnitStates(ctx context.Context, filter *pb.UnitStateFilter) (*pb.UnitStates, error) {
-	fmt.Println("XENGINE", "GetUnitStates", filter)
+	defer debug.Exit_(debug.Enter_())
+
 	states := s.localRegistry.UnitStates()
 
 	return &pb.UnitStates{states}, nil
 }
 
 func (s *rpcserver) ClearUnitHeartbeat(ctx context.Context, name *pb.UnitName) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "ClearUnitHeartbeat", name)
+	defer debug.Exit_(debug.Enter_(name.Name))
+
 	s.localRegistry.ClearUnitHeartbeat(name.Name)
 	return &pb.GenericReply{}, nil
 }
 
 func (s *rpcserver) CreateUnit(ctx context.Context, u *pb.Unit) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "CreateUnit", u)
+	defer debug.Exit_(debug.Enter_(u.Name))
+
 	err := s.etcdRegistry.CreateUnit(rpcUnitToJobUnit(u))
 	if err == nil {
 		s.localRegistry.CreateUnit(u)
@@ -112,7 +117,8 @@ func (s *rpcserver) CreateUnit(ctx context.Context, u *pb.Unit) (*pb.GenericRepl
 }
 
 func (s *rpcserver) DestroyUnit(ctx context.Context, name *pb.UnitName) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "DestroyUnit", name)
+	defer debug.Exit_(debug.Enter_(name.Name))
+
 	err := s.etcdRegistry.DestroyUnit(name.Name)
 	if err == nil {
 		s.localRegistry.DestroyUnit(name.Name)
@@ -121,25 +127,29 @@ func (s *rpcserver) DestroyUnit(ctx context.Context, name *pb.UnitName) (*pb.Gen
 }
 
 func (s *rpcserver) UnitHeartbeat(ctx context.Context, heartbeat *pb.Heartbeat) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "Heartbeat", heartbeat)
+	defer debug.Exit_(debug.Enter_(heartbeat))
+
 	s.localRegistry.UnitHeartbeat(heartbeat.Name, heartbeat.Machine, time.Duration(heartbeat.TTL)*time.Second)
 	return &pb.GenericReply{}, nil
 }
 
 func (s *rpcserver) RemoveUnitState(ctx context.Context, name *pb.UnitName) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "RemoveUnitState", name)
+	defer debug.Exit_(debug.Enter_(name.Name))
+
 	s.localRegistry.RemoveUnitState(name.Name)
 	return &pb.GenericReply{}, nil
 }
 
 func (s *rpcserver) SaveUnitState(ctx context.Context, req *pb.SaveUnitStateRequest) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "SaveUnitState", req)
+	defer debug.Exit_(debug.Enter_(req))
+
 	s.localRegistry.SaveUnitState(req.Name, req.State, time.Duration(req.TTL)*time.Second)
 	return &pb.GenericReply{}, nil
 }
 
 func (s *rpcserver) ScheduleUnit(ctx context.Context, unit *pb.ScheduleUnitRequest) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "ScheduleUnit", unit)
+	defer debug.Exit_(debug.Enter_(unit.Name, unit.Machine))
+
 	err := s.etcdRegistry.ScheduleUnit(unit.Name, unit.Machine)
 	if err == nil {
 		s.localRegistry.ScheduleUnit(unit.Name, unit.Machine)
@@ -149,7 +159,8 @@ func (s *rpcserver) ScheduleUnit(ctx context.Context, unit *pb.ScheduleUnitReque
 }
 
 func (s *rpcserver) SetUnitTargetState(ctx context.Context, unit *pb.ScheduledUnit) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "SetUnitTargetState", unit)
+	defer debug.Exit_(debug.Enter_(unit.Name, unit.CurrentState))
+
 	err := s.etcdRegistry.SetUnitTargetState(unit.Name, rpcUnitStateToJobState(unit.CurrentState))
 	if err == nil {
 		if s.localRegistry.SetUnitTargetState(unit.Name, unit.CurrentState) {
@@ -160,7 +171,8 @@ func (s *rpcserver) SetUnitTargetState(ctx context.Context, unit *pb.ScheduledUn
 }
 
 func (s *rpcserver) UnscheduleUnit(ctx context.Context, unit *pb.UnscheduleUnitRequest) (*pb.GenericReply, error) {
-	fmt.Println("XENGINE", "UnscheduleUnit", unit)
+	defer debug.Exit_(debug.Enter_(unit.Name, unit.Machine))
+
 	err := s.etcdRegistry.UnscheduleUnit(unit.Name, unit.Machine)
 	if err == nil {
 		s.localRegistry.UnscheduleUnit(unit.Name, unit.Machine)
@@ -176,6 +188,8 @@ func (s *rpcserver) Identify(ctx context.Context, props *pb.MachineProperties) (
 }
 
 func (s *rpcserver) AgentEvents(props *pb.MachineProperties, stream pb.Registry_AgentEventsServer) error {
+	defer debug.Exit_(debug.Enter_(props.Id))
+
 	s.mu.Lock()
 	ch := make(machineChan)
 	s.machinesDirectory[strings.ToLower(props.Id)] = ch
