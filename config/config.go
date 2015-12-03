055241353baa22b22e9101cb0d297b309ac48733
Hugo Tavares Reis <hugo.t.r@gmail.com>
2015-12-03 16:04:13 +0100

    added capabilities

--- config/config.go
+++ config/config.go
@@ -16,6 +16,8 @@ package config
 
 import (
 	"strings"
+
+	"github.com/coreos/fleet/machine"
 )
 
 type Config struct {
@@ -33,8 +35,16 @@ type Config struct {
 	TokenLimit              int
 	DisableEngine           bool
 	DisableWatches          bool
+	EnableGRPC              bool
 	VerifyUnits             bool
 	AuthorizedKeysFile      string
+}
+
+func (c *Config) Capabilities() machine.Capabilities {
+	return machine.Capabilities{
+		machine.CapENGINE: !c.DisableEngine,
+		machine.CapGRPC:   c.EnableGRPC,
+	}
 }
 
 func (c *Config) Metadata() map[string]string {
