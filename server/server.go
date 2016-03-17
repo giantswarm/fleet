6d18c6c3b11d776a19971d593f382e8c1408a211
Hector Fernandez <hectorj@gmail.com>
2016-03-17 12:19:53 +0100

    Disabled engine checks leader grpc capabilities

--- server/server.go
+++ server/server.go
@@ -138,7 +138,7 @@ func New(cfg config.Config) (*Server, error) {
 	} else {
 		regMux := genericReg.(*rpc.RegistryMux)
 		e = engine.New(reg, lManager, rStream, mach, regMux.EngineChanged)
-		if cfg.DisableEngine {
+		if cfg.DisableEngine && e.IsGrpcLeader() {
 			regMux.ConnectRPCRegistry()
 		}
 	}
